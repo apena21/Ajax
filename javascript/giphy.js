81b936b1c3b69f758e68773d79e516d1dc5533c5
Anna P <anna_p@mail.com>
2018-01-15 20:41:33 -0500

    fourth commit

--- javascript/giphy.js
+++ javascript/giphy.js
@@ -1,10 +1,8 @@
-// To do: ask tutor about styling gifs so that more than one appears in the same row instead of appearing vertically. 
-// ask how to limit request and response to only "still" and "animated gifs"
-// ask how to implement the switch between and a still and an animated gif with a button click.  
-// 
+// Review ask how to limit request and response to only "still" and "animated gifs"
+// Review ask how to implement the switch between and a still and an animated gif with a button click.  
 
-//create array of strings:
 
+// create array of strings:
 var sportsTypes = [    
   "swimming",
   "football",
@@ -16,259 +14,136 @@ var sportsTypes = [
   "handball",
   "ice skating",
   "golf",
-  ]
+];
     
-      
-  // call renderButtons function
-  renderButtons()
+// call renderButtons function
+renderButtons();
 
-  /*
-    
-  // The createButton function takes data returned by Giphy and appends a button for each string in the array.
-  function createButton(data) {
-  // Append the newly created button to the page
-  button.append(topics[i]);
-  console.log(button);
-    
-  }
-  */
-    
+/*
+  
+// The createButton function takes data returned by Giphy and appends a button for each string in the array.
+function createButton(data) {
+// Append the newly created button to the page
+button.append(topics[i]);
+console.log(button);
+}
+*/
+
+// This is required to add a new sport, which only adds a new
+// button to the array, and calls renderButtons() method.
 $("#button").on("click", function (event) {
+  event.preventDefault();
 
-event.preventDefault();
-// takes out the text from the user's value
-//$("#textInput").val()
-    
-// takes out the text from the user's value 
-
-//and puts it into the array.
-
+  // takes out the text from the user's value
+  // and puts it into the array.
   sportsTypes.push($("#textInput").val());
   renderButtons();
-
-  //adding the following code on 1/12
-  // Grabbing and storing the sports value from the button
+});
+    
+       
+// end of new 1/12/ code    
+// listening for clicks for any button that is inside the sports id. Added an extra CSS selector "button" to identify the    
+// specific thing inside the sports div that was clicked.  Listen on the parent element to always be able to catch    
+// clicks from dynamically added buttons that did not exist when page was loaded. The Submit button exists the whole    
+// time so it's a good one to listen on. This is known as "Event Delegation".  Delegate the listening from something that may    
+// or may not exist to something that exists the whole time.
+    
+$("#sports").on("click", "button", function (event) {
+  // event is a jquery property, .text is a Jquery method    
+  // including something in the () passing the variable into the function
+  // see http://api.jquery.com/text/
+    
+  var userText = $(event.target).text();
+  // added 1/12
+  // Grabbing and storing the sports value from the button    
   var query = $(this).attr("data-name");
-         
-  // Constructing a queryURL using the sport name
-  var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=yXpUFSBSyo5ZTKljdu245PqedaVENLh3&q=" + query + "&limit=10&rating=pg-13";
-    
+  
+  // Constructing a queryURL using the sport name    
+  var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=yXpUFSBSyo5ZTKljdu245PqedaVENLh3&q=" + query + "&limit=10&rating=pg";
+  
   // Performing an AJAX request with the queryURL
-    
   $.ajax({
-    url: queryURL,
-    method: "GET"
-    })
-    
-    // After data comes back from the request
-    .then(function(response) {
-    console.log(queryURL);
+    url: queryURL,    
+    method: "GET"    
+  })
+  // After data comes back from the request
+  .then(function(response) {
+    console.log(queryURL);  
     console.log(response);
-
+  
     // storing the data from the AJAX request in the results variable
     var results = response.data;
-
+  
     // Looping through each result item
     for (var i = 0; i < results.length; i++) {
-    
-    // Creating and storing a div tag
-    var sportsDiv = $("<div>");
-
-    // Creating a paragraph tag with the result item's rating
-    var p = $("<p>").text("Rating: " + results[i].rating);
-    
-    //added to distinguish between still and animated images
-    // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
-    var state = sportsImage.attr("preview_gif" || "original_still");
-    
-    // Creating and storing an image tag
-    var sportsImage = $("<img>");
-    
-    // Setting the src attribute of the image to a property pulled off the result item with animation
-    sportsImage.attr("src", results[i].images.original_still);
-    console.log(results[i].images.original_still);
-
-    // source for still image
-    //sportsImage.attr("src", results[i].images.fixed_height.url, "data-still");
-
-    if (state === "data-animate") {
-
-    $(this).attr("src", sportsImage.attr("preview_gif"));
-
-    } else {
-
-    $(this).attr("src", sportsImage.attr("original_still"));
-
-    }
-
-    // Appending the paragraph and image tag to the sportsDiv
-
-    sportsDiv.append(p);
-    sportsDiv.append(sportsImage);
-
-    // Prependng the sportsDiv to the HTML page in the "#gifs-appear-here" div
-    $("#gifs-appear-here").prepend(sportsDiv);
-
-  }
-
-});
-    
-});
-    
-        
-    // end of new 1/12/ code    
-    // listening for clicks for any button that is inside the sports id. Added an extra CSS selector "button" to identify the    
-    // specific thing inside the sports div that was clicked.  Listen on the parent element to always be able to catch    
-    // clicks from dynamically added buttons that did not exist when page was loaded. The Submit button exists the whole    
-    // time so it's a good one to listen on. This is known as "Event Delegation".  Delegate the listening from something that may    
-    // or may not exist to something that exists the whole time.
-    
-    $("#sports").on("click", "button", function (event) {
-    
-        // event is a jquery property, .text is a Jquery method    
-        // including something in the () passing the variable into the function
-        // see http://api.jquery.com/text/
-    
-    var userText = $(event.target).text();
-     /*
-    
-    //call function and pass the parameter;
-    searchGiphyAPI(userText);
-
-     */
-    
-     
-    
-    //added 1/12
-    // Grabbing and storing the sports value from the button    
-    var query = $(this).attr("data-name");
-    
-    // Constructing a queryURL using the sport name    
-    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=yXpUFSBSyo5ZTKljdu245PqedaVENLh3&q=" + query + "&limit=10&rating=pg";
-  
-    // Performing an AJAX request with the queryURL
-    
-    $.ajax({
-  
-        url: queryURL,    
-        method: "GET"    
-    })
-  
-    // After data comes back from the request
-  
-      .then(function(response) {
-  
-        console.log(queryURL);  
-        console.log(response);
-  
-    // storing the data from the AJAX request in the results variable
-      var results = response.data;
-    
-      // Looping through each result item
-  
-      for (var i = 0; i < results.length; i++) {
-      
       // Creating and storing a div tag
       var sportsDiv = $("<div>");
+      //use the .gif class in the css file to style & align the gifs.
+      sportsDiv.addClass("gif");
       
       // Creating a paragraph tag with the result item's rating    
-      var p = $("<p>").text("Rating: " + results[i].rating);
+      var rating = $("<div>").text("Rating: " + results[i].rating);
+      // use the .rating class in the css file to style & align the ratings for each gif.
+      rating.addClass("rating");
 
       // Creating and storing an image tag
       var sportsImage = $("<img>");
 
       // Setting the src attribute of the image to a property pulled off the result item
-      sportsImage.attr("src", results[i].images.fixed_height.url);
+      sportsImage.attr("src", results[i].images.fixed_height_still.url);
+
+      // ********************************************************
+      // Pausing functionality **********************************
+      sportsImage.attr("data-animate", results[i].images.fixed_height.url);
+      sportsImage.attr("data-still", results[i].images.fixed_height_still.url);
+      sportsImage.attr("data-state", "still"); // show still gif initially
+
+      sportsImage.on("click", function() {
+        var state = $(this).attr("data-state");
+        if(state === "animate") {
+          $(this).attr("src", $(this).attr("data-still"));
+          $(this).attr("data-state", "still");
+        }
+        else {
+          $(this).attr("src", $(this).attr("data-animate"));
+          $(this).attr("data-state", "animate");
+        }
+      });
+      // Pausing functionality done *****************************
+      // ********************************************************
     
       // Appending the paragraph and image tag to the sportsDiv
-      sportsDiv.append(p);
+      sportsDiv.append(rating);
       sportsDiv.append(sportsImage);
 
       // Prependng the sportsDiv to the HTML page in the "#gifs-appear-here" div
       $("#gifs-appear-here").prepend(sportsDiv);
-
-
-
     }
   });
 });
     
-  // Function for displaying sport button    
-  function renderButtons() {
-
+// Function for displaying sport button    
+function renderButtons() {
   // Deleting the sport buttons prior to adding new sport buttons    
   // to prevent having repeat buttons    
   $("#sports").empty();
 
   // Looping through the array of topics
   for (var i = 0; i < sportsTypes.length; i++) {
+    // Then dynamically generate buttons for each sport in the array.
+    // This code $("<button>") is all jQuery needs to create the start and end tag. (<button></button>)
+    var a = $("<button>");
 
-  // Then dynamically generate buttons for each sport in the array.
-  // This code $("<button>") is all jQuery needs to create the start and end tag. (<button></button>)
-  var a = $("<button>");
+    // Adding a class
+    a.addClass("sport");
 
-  // Adding a class
-  a.addClass("sport");
+    // Adding a data-attribute with a value of the sport at index i
+    a.attr("data-name", sportsTypes[i]);
 
-  // Adding a data-attribute with a value of the sport at index i
-  a.attr("data-name", sportsTypes[i]);
-
-  // Providing the button's text with a value of the sport at index i
-  a.text(sportsTypes[i]);
-  
-  // Adding the button to the HTML
-  $("#sports").append(a);
-
+    // Providing the button's text with a value of the sport at index i
+    a.text(sportsTypes[i]);
+    
+    // Adding the button to the HTML
+    $("#sports").append(a);
   }
 }
-    
-  /*
-
-function searchGiphyAPI(query) {
-
-    
-
-    // Did I construct the url correctly?
-
-    // Example queryURL for Giphy API 
-
-    const queryURL = "https://api.giphy.com/v1/gifs/search?api_key=yXpUFSBSyo5ZTKljdu245PqedaVENLh3&q=" + query + "&limit=10&rating=pg";
-
-    
-
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-      //.done expects to be passed a function or you will get an undefined error.
-
-    }).done(function (response) {
-
-        console.log (response);
-
-    });
-}
-
-  
-
-// displaying results to user will be similar to the create button, except will create an image instead of a button.
-
-// remember to clear the div before appending them so that you replace new pics from a fresh button click with old ones.
-
-  
-
-//searchGiphyAPI("");
-
-
-/*
-
-  for(var i = 0; i < topics.length; i++){
-
-    //this will print names of all the sports
-
-    console.log(sportTypes[i]);
-
-    //create buttons using the printed string
-
-    buttons[i] = $("<a href='#' data-theme='b' data-role='button'>" + sportTypes[i] + "</a>");      
-    
-    */
